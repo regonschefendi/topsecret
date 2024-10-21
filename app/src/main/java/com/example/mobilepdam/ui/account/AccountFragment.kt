b766abd53e5cbee65cde0e9c5d9aee59d1d91109
mraihannayuna <m.raihannayua13@gmail.com>
2024-10-21 11:23:20 +0700

    akun not done

--- app/src/main/java/com/example/mobilepdam/ui/account/AccountFragment.kt
+++ app/src/main/java/com/example/mobilepdam/ui/account/AccountFragment.kt
@@ -1,20 +1,16 @@
-package com.example.mobilepdam.ui.dashboard
+package com.example.mobilepdam.ui.account
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.example.mobilepdam.databinding.FragmentDashboardBinding
+import com.example.mobilepdam.databinding.FragmentAccountBinding
 
-class DashboardFragment : Fragment() {
+class AccountFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentAccountBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -23,7 +19,7 @@ class DashboardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentAccountBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         return root
